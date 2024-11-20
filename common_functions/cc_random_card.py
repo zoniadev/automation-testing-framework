f87acb641effd8c211d5391240ea4b60b9e9e58c
nicktodorov <nicktodorov@gmail.com>
2024-11-20 16:40:11 +0200

    different cc

--- common_functions/cc_random_card.py
+++ common_functions/cc_random_card.py
@@ -3,15 +3,8 @@ import common_variables
 
 
 def pick_payment_card():
-    remaining_cards = common_variables.test_cards.copy()
-    if not remaining_cards:
-        common_variables.used_cards = []
-        remaining_cards = common_variables.test_cards.copy()
-
-    selected_card = random.choice(remaining_cards)
-    remaining_cards.remove(selected_card)
-    common_variables.used_cards.append(selected_card)
-
+    selected_card = common_variables.test_cards[common_variables.card_index]
     common_variables.test_cc_number = selected_card["number"]
     common_variables.test_cc_type = selected_card["type"]
     print(f'Scenario will use "{common_variables.test_cc_type}" card "{common_variables.test_cc_number}"')
+    common_variables.card_index = (common_variables.card_index + 1) % len(common_variables.test_cards)
