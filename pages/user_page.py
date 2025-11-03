21de3de6b56671a4c4d03744adafda5a68faec7d
Nikolay Kalendzhiev <nikolay.kalendzhiev@primeholding.com>
2025-11-03 11:08:17 +0200

    fix new flow

--- pages/user_page.py
+++ pages/user_page.py
@@ -8,11 +8,6 @@ class UserPage(BasePage):
     def __init__(self, context):
         super().__init__(context)
 
-    def skip_questions(self):
-        time.sleep(0.5)
-        self.context.page.locator(SKIP_QUESTIONS_BUTTON).click()
-        print('===> Skipped questions')
-
     def verify_registration(self):
         name_on_page = self.context.page.locator(USER_MENU_BUTTON).inner_text()
         assert common_variables.supplement_funnel_name in name_on_page, (f'Expected name to be '
