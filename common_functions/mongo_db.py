721628a44e018cc69efdb5d2bdc2edfd18fd8daa
Nikolay Kalendzhiev <nicktodorov@gmail.com>
2026-09-23 13:02:30 +0300

    new mail works

--- common_functions/mongo_db.py
+++ common_functions/mongo_db.py
@@ -6,7 +6,7 @@ def clean_automation_users_with_api():
     payload = ""
     headers = {
         'Content-Type': 'application/json',
-        'X-Automation-Cleanup-Key': 'x6qgUsY4sDhtFNSiS0KeOP8MHY0sGFYBVWJzcs2Ycic1',
+        'X-Automation-Cleanup-Key': 'x6qgUsY4sDhtFNSiS0KeOP8MHY0sGFYBVWJzcs2Ycic',
         }
 
     response = requests.request("POST", url, headers=headers, data=payload)
